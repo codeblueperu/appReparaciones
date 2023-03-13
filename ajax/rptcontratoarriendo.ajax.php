e908c1b8870d66a1f9b55f43ad12e4cbc48d8e1b
335983200 <jhoningaaquino@gmail.com>
2023-03-13 16:46:19 -0500

    OneVersion

--- ajax/rptcontratoarriendo.ajax.php
+++ ajax/rptcontratoarriendo.ajax.php
@@ -1,8 +1,9 @@
 <?php
-include_once  '../extensiones/dompdf/autoload.inc.php';
+include_once '../extensiones/dompdf/autoload.inc.php';
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
+
 ob_start();
 $options = new Options();
 $options->set('isRemoteEnabled', true);
@@ -13,14 +14,45 @@ $dompdf = new DOMPDF($options);
 
 // Componer el HTML
 $html = '
+<style>
+    @page {
+        margin-left: 0.7cm;
+        margin-right: 0.7cm;
+        margin-top: 0.7cm;
+    }
+    p{
+        width:100%;
+    }
+    .content_cliente{
+        width:100%;
+        position: relative;
+    }
+     span.p_don{
+        width: 200px;
+        color:red;
+        position:absolute;
+    }
+</style>
 <body>
     <div class="container-fluid">
-        <div class="banner">
-       
-            <img src="'.$_SERVER['DOCUMENT_ROOT'].'/fyfsistema/ajax/banner.png" alt="">
+        <div class="banner">       
+                    
         </div>
-        '. $_SERVER['DOCUMENT_ROOT'].'
-        ola
+
+        <div class="content_fecha">
+            <p>En antofagasta 	<b>13</b>	de	<b>Marzo</b>			del <b>2023</b>	Nº	<b>2460</b></p>
+
+            <p style="text-align:justify;font-size:12px">Entre SERVICIOS GENERALES MEDINA ASOCIADOS SPA., RUT: 76,957462-K en adelante "EL ARRENDADOR" debidamente representado
+             por el Sr. CARLOS MEDINA IZQUIERDO Rep. Legal, domiciliados en Huamachuco 9962, Antofagasta - Cel.: 9 9264 4118 por una parte y la otra.</p>										
+
+        </div>
+
+        <div class="content_cliente">
+            <p><span style="width:150px;">Don</span> <b>: More Inga Jhony</b> <span>RUT</span> <b>013587469</b></p> 
+            <p><span>Domicilio</span> <b>: Jr meliton carbajal #240 SMP</b> <span>Ciudad</span> <b>: Lima</b> <span>Fono</span> <b>: 963258741</b></p> 
+        </div>
+       
+        
     </div>
 </body>
 ';
@@ -33,5 +65,5 @@ $dompdf->render();
 /*header("Content-type: application/pdf");
 header("Content-Disposition: inline; filename=comprobante");
 echo $dompdf->output(); */
-$dompdf->stream("", array("Attachment" => false));
+$dompdf->stream("comprobante", array("Attachment" => false));
 ?>
